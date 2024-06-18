0f15f2d4c79e5f9360a29749ccc3ca548aabff4e
Michael Filoramo <mlfiloramo@gmail.com>
2024-06-17 20:19:56 -0400

    Fix exclusion of weeks 1, 2, 3

---
+++ 01-Class-Content/04-Web-APIs/01-Activities/02-Stu_Window-Object/Unsolved/script.js
@@ -0,0 +1,12 @@
+// LOGS THE .....
+console.log(window);
+
+// TODO: What will the following line of code log?
+console.log(window.document);
+
+// TODO: What will the following line of code log?
+console.log(document.documentElement);
+
+// TODO: What will the following line of code log?
+console.log(document.head);
+
