77df3cb6a7a597a9e11552d987651bb850808e68
Michael Filoramo <mlfiloramo@gmail.com>
2024-06-18 01:03:36 -0400

    Add Solved materials for Web APIs + demo

---
+++ 01-Class-Content/04-Web-APIs/01-Activities/14-Stu_Preventing-Default/Solved/assets/js/script.js
@@ -0,0 +1,27 @@
+const tipEl = document.querySelector('#tip-percentage');
+const totalEl = document.querySelector('#total');
+const submitEl = document.querySelector('#submit');
+
+function calculateTip(total, tipPercentage) {
+  const roundedResult = (total * tipPercentage).toFixed(2);
+  return roundedResult;
+}
+
+function calculateTotal(total, tipAmount) {
+  return parseFloat(total) + parseFloat(tipAmount);
+}
+
+function addTip(event) {
+  event.preventDefault();
+  const tipPercentage = tipEl.value * 0.01;
+  const total = totalEl.value;
+  const tipAmount = calculateTip(total, tipPercentage);
+  const newTotal = calculateTotal(tipAmount, total);
+  // Update HTML with tip and total
+  document.querySelector('#tip-amount').textContent = tipAmount;
+  // Use toFixed() to limit to two decimal places
+  document.querySelector('#new-total').textContent = newTotal.toFixed(2);
+}
+
+// Add listener to submit element
+submitEl.addEventListener('click', addTip);
