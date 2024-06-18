68c4649b8b4caba9af87b871183bcd193a36f622
Michael Filoramo <mlfiloramo@gmail.com>
2024-06-17 22:59:04 -0400

    Add demo

---
+++ 01-Class-Content/04-Web-APIs/01-Activities/11-Ins_Event-Listener/assets/js/script.js
@@ -0,0 +1,11 @@
+// Access toggle switch HTML element
+const themeSwitcher = document.querySelector('#theme-switcher');
+const container = document.querySelector('.container');
+
+// Set default mode to dark
+let mode = 'dark';
+
+// Listen for a click event on toggle switch
+themeSwitcher.addEventListener('click', (text) => {
+  console.log(text);
+});
