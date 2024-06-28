b137fadbbb347579b7294bb90b7191de14afdfea
Michael Filoramo <mlfiloramo@gmail.com>
2024-06-28 08:34:35 -0400

    Add Solved materials for Lesson 6.1

---
+++ 01-Class-Content/06-Server-Side-APIs/01-Activities/08-Stu_Parse_JSON/Solved/assets/js/script.js
@@ -0,0 +1,13 @@
+const requestUrl = 'https://api.github.com/repos/twitter/chill/issues?per_page=5';
+
+fetch(requestUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log('Github Repo Issues \n----------');
+    for (let i = 0; i < data.length; i++) {
+      console.log(data[i].url);
+      console.log(data[i].user.login);
+    }
+  });
